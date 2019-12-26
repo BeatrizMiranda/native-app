6eb6c98a7d0ceb4673d3a03f47558a21cfbccf67
Beatriz Miranda <beatriz.miranda@acct.global>
2019-12-26 18:03:03 -0300

    context

---
+++ contextapp/src/Components/BookList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() { 
+        return ( 
+            <div className="booklist">
+                <ul>
+                    <li>The fear of a wise man</li>
+                    <li>The name of the wind</li>
+                    <li>The sound of silence</li>
+                </ul>
+
+            </div>
+         );
+    }
+}
+ 
+export default BookList;
