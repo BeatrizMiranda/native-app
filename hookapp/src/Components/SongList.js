ebd7ef183605fab797fe689cce8643e365d55d01
Beatriz Miranda <beatriz.miranda@acct.global>
2019-12-27 14:38:37 -0300

    hook

--- hookapp/src/Components/SongList.js
+++ hookapp/src/Components/SongList.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import uuid from 'uuid/v1';
 
 const SongList = () => {
+    const [songs, setSongs] = useState([
+        {title: 'Silhouette', id: 1},
+        {title: 'Busyhead', id: 2},
+        {title: 'Havana', id: 3}
+    ]);
+
+    const addSong = () => {
+        setSongs([...songs, {title: 'new song', id: uuid() }]);
+    }
+
     return ( 
         <div className="songList">
             <ul>
-                <li>Silhouette</li>
-                <li>Busyhead</li>
-                <li>Havana</li>
+                {songs.map(song => {
+                    return (<li key={song.id} >{song.title}</li>)
+                })}
             </ul>
+            <button className="add" onClick={addSong}>Add song</button>
         </div>
     );
 }
