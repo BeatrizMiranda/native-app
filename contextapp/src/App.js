bf5062f4330441914b4974fe3ddd6ae63f3fa8c9
Beatriz Miranda <beatriz.miranda@acct.global>
2019-12-27 13:19:40 -0300

    removing unecessary image and adding theme context

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import NavBar from './Components/NavBar';
 import BookList from './Components/BookList';
 import ThemeContextProvider from './Contexts/ThemeContext'
+import ThemeToggle from './Components/ThemeToggle';
+import AuthContextProvider from './Contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <NavBar />
-        <BookList />
-      </ThemeContextProvider>
+      <AuthContextProvider>
+
+        <ThemeContextProvider>
+
+          <NavBar />
+          <BookList />
+          <ThemeToggle />
+
+        </ThemeContextProvider>
+
+      </AuthContextProvider>
     </div>
   );
 }
-
 export default App;
--- contextapp/src/Components/BookList.js
+++ contextapp/src/Components/BookList.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../Contexts/ThemeContext';
 
 class BookList extends Component {
+    //import all data from context Teacher prefere this one (but cant use two differents consumer)
+
+    static contextType = ThemeContext
     render() { 
+        const {isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return ( 
-            <div className="booklist">
+            <div className="booklist" style={{background: theme.bg, color: theme.syntax}}>
                 <ul>
-                    <li>The fear of a wise man</li>
-                    <li>The name of the wind</li>
-                    <li>The sound of silence</li>
+                    <li style={{background: theme.ui}}>The fear of a wise man</li>
+                    <li style={{background: theme.ui}}>The name of the wind</li>
+                    <li style={{background: theme.ui}}>The sound of silence</li>
                 </ul>
 
             </div>
          );
     }
 }
+
  
 export default BookList;
