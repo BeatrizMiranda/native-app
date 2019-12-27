b1d9a743de97c2b6967a805bb60841603c5af530
Beatriz Miranda <beatriz.miranda@acct.global>
2019-12-27 14:06:14 -0300

    init repository

---
+++ hookapp/src/Components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SongList = () => {
+    return ( 
+        <div className="songList">
+            <ul>
+                <li>Silhouette</li>
+                <li>Busyhead</li>
+                <li>Havana</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default SongList;
